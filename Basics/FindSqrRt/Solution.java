f90abccfe076d5cb88569660d79c45e8cc2a597f
intellij <patilprajwal8804@gmail.com>
2025-01-06 12:10:06 +0530

    done

---
+++ Basics/FindSqrRt/Solution.java
@@ -0,0 +1,15 @@
+package Basics.FindSqrRt;
+import java.math.*;
+public class Solution {
+    public int mySqrt(int x) {
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        double num=36;
+        int sqrt= (int) Math.sqrt(num);
+        System.out.println(sqrt);
+    }
+}
