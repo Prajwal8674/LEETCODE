05f83a42d85e3047f0f9e51871b563a8cd76b3c9
intellij <patilprajwal8804@gmail.com>
2025-10-05 02:30:55 +0530

    done

---
+++ Array/EveOddNoOnEveOddIndx/Solution.java
@@ -0,0 +1,27 @@
+package Array.EveOddNoOnEveOddIndx;
+////////////////////////////////////// pending //////////////////
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void shift(int arr[] ){
+        int index = 0;
+        int num=0;
+        for(int i = 0 ; i < arr.length ; i++){
+
+            if(i%2==0 && arr[i]%2!=0){
+
+
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] ={2,4,5,7};
+
+        shift(arr);
+    }
+
+}
