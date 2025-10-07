5a6bc311a1740c08239f54ddbc497ad809985970
intellij <patilprajwal8804@gmail.com>
2025-10-07 21:04:05 +0530

    done

---
+++ Array/LetterCombiOfPhnNo/Solution.java
@@ -0,0 +1,14 @@
+package Array.LetterCombiOfPhnNo;
+
+public class Solution {
+
+    public static void checkCombi(int num){
+        String[] chars = {"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+    }
+    public static void main(String[] args) {
+
+        int num = 234;
+        checkCombi(num);
+    }
+}
