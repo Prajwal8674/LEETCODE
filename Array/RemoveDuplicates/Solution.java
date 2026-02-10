fdc6ea36d88df64526aef04092fa6fc9b69d6256
intellij <patilprajwal8804@gmail.com>
2026-02-10 23:46:20 +0530

    done

---
+++ Array/RemoveDuplicates/Solution.java
@@ -0,0 +1,35 @@
+package Array.RemoveDuplicates;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static int removeDuplicate(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 0;
+        int j = 1;
+
+        while (j < nums.length) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+            j++;
+        }
+
+        return i + 1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+
+        int newLength = removeDuplicate(nums);
+
+        System.out.println("New length: " + newLength);
+        System.out.print("Unique elements: ");
+        int k = 0;
+        while (k < newLength) {
+            System.out.print(nums[k] + " ");
+            k++;
+        }
+    }
+}
