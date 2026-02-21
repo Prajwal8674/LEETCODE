f4723a26b70fa44be7a89cd5bdcfebaacda6c5ab
intellij <patilprajwal8804@gmail.com>
2026-02-21 22:23:45 +0530

    done

---
+++ String/LongestNonRepSubStr/Solution.java
@@ -0,0 +1,30 @@
+package String.LongestNonRepSubStr;
+
+import java.util.HashSet;
+
+public class Solution {
+        public static int lengthOfLongestSubstring(String s) {
+            int left = 0;
+            int maxLength = 0;
+
+            HashSet<Character> charSet = new HashSet<>();
+
+            for (int right = 0; right < s.length(); right++) {
+                while (charSet.contains(s.charAt(right))) {
+                    charSet.remove(s.charAt(left));
+                    left++;
+                }
+
+                charSet.add(s.charAt(right));
+                maxLength = Math.max(maxLength, right - left + 1);
+            }
+
+            return maxLength;
+        }
+
+    public static void main(String[] args) {
+
+        String str="abcabcbb";
+        System.out.println(lengthOfLongestSubstring(str));
+    }
+}
