f5ff7269670a67d054dd385ac81b4cd5a71b851b
intellij <patilprajwal8804@gmail.com>
2026-02-27 23:53:26 +0530

    done

---
+++ Array/MergeTwoSortedArray/Solution.java
@@ -0,0 +1,19 @@
+package Array.MergeTwoSortedArray;
+
+public class Solution {
+
+    public static void merge(int[] arr1, int a1, int[] arr2, int a2) {
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr1= {1,2,3,0,0,0};
+        int[] arr2={2,5,6};
+        int a1 = arr1.length;
+        int a2 = arr2.length;
+
+        merge(arr1,a1,arr2,a2);
+    }
+}
