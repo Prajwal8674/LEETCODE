838fdf14ce015bccb80bbdcc05a6c1333865f18e
intellij <patilprajwal8804@gmail.com>
2026-03-06 01:58:51 +0530

    done

---
+++ Basics/ThirdMax/Solution.java
@@ -0,0 +1,29 @@
+package Basics.ThirdMax;
+
+public class Solution {
+
+    public static int thirdMax(int[] nums) {
+        long max1 = Long.MIN_VALUE, max2 = Long.MIN_VALUE, max3 = Long.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2 && num != max1) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3 && num != max1 && num != max2) {
+                max3 = num;
+            }
+        }
+        return (max3 == Long.MIN_VALUE) ? (int)max1 : (int)max3;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,1};
+        int a = thirdMax(arr);
+        System.out.println(a);
+    }
+}
