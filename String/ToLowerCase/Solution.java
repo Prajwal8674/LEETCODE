683cbbe6db891b472a7ffd7c95de7cba92f3833b
intellij <patilprajwal8804@gmail.com>
2026-03-07 22:59:34 +0530

    done

--- String/ToLowerCase/Solution.java
+++ String/ToLowerCase/Solution.java
@@ -1,21 +1,24 @@
 package String.ToLowerCase;
-
 public class Solution {
+
 
     public static String toLowerCase(String str) {
 
-        String ans=" ";
-        char ch = str.charAt(i);
-        for( int i = 0; i< str.length() ;i++){
-            if(str.charAt(i)>='A' &&str.charAt(i)<='Z'){
+        StringBuilder ans = new StringBuilder();
 
-                ch = (char)(ch+32);
+        for(int i = 0; i < str.length(); i++){
+
+            char ch = str.charAt(i);
+
+            if(ch >= 'A' && ch <= 'Z'){
+                ch = (char)(ch | 32);
             }
-            ans+=ch;
-        }
-        return  ans;
-    }
 
+            ans.append(ch);
+        }
+
+        return ans.toString();
+    }
     public static void main(String[] args) {
 
         String str = "Hello";
