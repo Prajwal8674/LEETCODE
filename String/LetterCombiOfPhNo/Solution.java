511539562339704c9ce670a26f3ba8c8d806e1b8
intellij <patilprajwal8804@gmail.com>
2026-03-09 01:30:30 +0530

    done

---
+++ String/LetterCombiOfPhNo/Solution.java
@@ -0,0 +1,47 @@
+package String.LetterCombiOfPhNo;
+
+import java.util.*;
+
+class Solution {
+
+    static String[] map = {
+            "", "", "abc", "def", "ghi",
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+
+    public  static List<String> letterCombinations(String digits) {
+
+        List<String> result = new ArrayList<>();
+
+        if(digits.length() == 0)
+            return result;
+
+        backtrack(digits, 0, new StringBuilder(), result);
+
+        return result;
+    }
+
+    public static void backtrack(String digits, int index, StringBuilder current, List<String> result){
+
+        if(index == digits.length()){
+            result.add(current.toString());
+            return;
+        }
+
+        String letters = map[digits.charAt(index) - '0'];
+
+        for(char c : letters.toCharArray()){
+
+            current.append(c);        // choose
+
+            backtrack(digits, index+1, current, result);  // explore
+
+            current.deleteCharAt(current.length()-1); // undo (backtrack)
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "23";
+        letterCombinations(str);
+    }
+}
