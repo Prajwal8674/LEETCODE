8bd656a82e8019730447c8257c30cfbe3d4768a9
intellij <patilprajwal8804@gmail.com>
2026-03-17 23:34:54 +0530

    done

---
+++ Basics/AddDigit/Solution.java
@@ -0,0 +1,17 @@
+package Basics.AddDigit;
+
+public class Solution {
+
+    public static int addDigits(int num) {
+
+        if (num == 0) {
+            return 0;
+        }
+        return 1 + (num - 1) % 9;
+    }
+    public static void main(String[] args) {
+
+        int num=38;
+        System.out.println(addDigits(num));
+    }
+}
