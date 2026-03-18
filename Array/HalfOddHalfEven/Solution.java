9ea592a941023c2779e269406f931a4885b52aa9
intellij <patilprajwal8804@gmail.com>
2026-03-18 22:44:05 +0530

    done

--- Array/HalfOddHalfEven/Solution.java
+++ Array/HalfOddHalfEven/Solution.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6};
-        
+
         int left = 0;
         int right = arr.length - 1;
 
+        
         while (left < right) {
             while (left < right && arr[left] % 2 != 0) {
                 left++;
