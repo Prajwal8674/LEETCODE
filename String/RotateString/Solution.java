c9c6b1fc1ea146e69626461cbdcd11d62c70c1d1
intellij <patilprajwal8804@gmail.com>
2026-03-22 01:57:24 +0530

    done

---
+++ String/RotateString/Solution.java
@@ -0,0 +1,24 @@
+package String.RotateString;
+
+public class Solution {
+
+    public static  boolean rotateString(String str, String goal) {
+
+        String s1 = str+str;
+
+
+        if(str.length()!=goal.length()){
+            return false;
+        }
+        if(s1.contains(goal)){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String str = "abcde";
+        String goal = "abced";
+        System.out.println(rotateString(str, goal));
+    }
+}
