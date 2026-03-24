e022b43e0bd95fdc8f7dce6a7d5f02444be585d6
intellij <patilprajwal8804@gmail.com>
2026-03-25 00:35:35 +0530

    done

---
+++ String/firstPalindrome/Solution.java
@@ -0,0 +1,31 @@
+package String.firstPalindrome;
+
+public class Solution {
+
+    public static boolean isPallindrome(String word){
+
+        int i = 0, j = word.length()-1;
+        while(i<j){
+            if(word.charAt(i++)!=word.charAt(j--)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String firstPalindrome(String[] words) {
+
+        for(String w : words){
+           if( isPallindrome(w)){
+               return w;
+           }
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+        String[] words = {"abc","car","ada","racecar","cool"};
+        System.out.println(firstPalindrome(words) );
+    }
+}
